1133cfc5a48ac7eb7dddb8ce9342d7daec841ea2
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-05-09 18:57:20 +0300

    update on default parameters

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,8 @@
 
 class BankAccount:
-    def __init__(self):
-        self.int_rate = 0.01
-        self.balance = 0
+    def __init__(self, int_rate=0.01, balance=0):
+        self.int_rate = int_rate
+        self.balance = balance
 
     def deposit(self, amount):
         self.balance += amount
