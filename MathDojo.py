2a7ef3f3c2ecfe3b95712326883764cfc42dc0cc
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-05-16 15:57:20 +0300

    zoo.py

--- MathDojo.py
+++ MathDojo.py
@@ -1,6 +1,8 @@
 class MathDojo:
     def __init__(self):
         self.result = 0
+# *args allows us to pass a variable number of non-keyword arguments to a Python function.
+#  In the function, we should use an asterisk ( * ) before the parameter name to pass a variable number of arguments.
 
     def add(self, num, *nums):
         self.result += num
